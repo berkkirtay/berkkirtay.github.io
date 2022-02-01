dd12a2ed22b1d565c5d6fc3c84e558847256392e
Berk Kırtay <berkberk24@gmail.com>
2022-02-01 21:06:13 +0300

    err fix 3

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import Header from "./components/header/Header";
 const App = () => {
   var blogs = [
     {
-      "_id": crypto.createHash("SHA1").update("On Cryptography").digest('base64').toLowerCase(),
+      "_id": crypto.createHash("SHA1").update("On Cryptography").digest("hex"),
       "title": "On Cryptography",
       "text": " Cryptography has a long history. The story starts with Caesar himself. He created the first known encryption method called Caesar Cipher. He used this method to send secret messages to his legions on the battlefield. Long story short, you can say his method was so easily breakable",
       "date": "November 04, 2021"
     },
     {
-      "_id": crypto.createHash("SHA1").update("My Thoughts on Genetic Algorithms").digest('base64').toLowerCase(),
+      "_id": crypto.createHash("SHA1").update("My Thoughts on Genetic Algorithms").digest('hex'),
       "title": "My Thoughts on Genetic Algorithms",
       "text": " Nowadays, everyone talks about artificial intelligence. We can say AI is almost the most hyped area of computer science. What about how AI works? What kind of algorithms do we use to make our computers perform those intelligent things?",
       "date": "November 19, 2021"
