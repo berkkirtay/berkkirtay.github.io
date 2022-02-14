492ae8bf08942ef0ea31ba063d6193f1ee482439
Berk Kırtay <berkberk24@gmail.com>
2022-02-14 23:58:08 +0300

    home page changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,9 @@ const Home = () => {
     return (
         <>
             <div className="aboutme">
-
                 <p style={{ paddingLeft: "1%", paddingRight: "1%" }}>
                     Hi, I am a junior Computer Science student at Marmara University. I write software and have been interested in computers since I was five. I love
-                    learning about anything related to computers and I am always eager to expand my expertise!
+                    learning about anything related to computer science and I am always eager to expand my expertise!
                 </p>
                 <hr />
                 <h4 style={{ paddingLeft: "1%" }}>
@@ -15,9 +14,9 @@ const Home = () => {
                 </h4>
                 <ul>
                     <li>Software Engineering</li>
-                    <p style={{ width: "85%", paddingLeft: "1%" }}>I am proficient about software development cycles and knowledgeable about famous practices that we apply to software. I try to write clean, modular and efficient software.</p>
+                    <p style={{ width: "85%", paddingLeft: "1%" }}>I am proficient about software development life cycles and knowledgeable about famous software engineering practices. I try to write clean, modular and efficient software.</p>
                     <li>Blockchain Technology</li>
-                    <p style={{ width: "85%", paddingLeft: "1%" }}>Blockchain is the technology of the future. I developed some basic applications of blockchain and several transaction systems. I am excited to see how much we can achieve with this technology!</p>
+                    <p style={{ width: "85%", paddingLeft: "1%" }}>Blockchain is the technology of the future. I developed some basic applications of blockchain and decentralized transaction systems. I am excited to see how much we can achieve with this technology!</p>
                     <li >Cryptography</li>
                     <p style={{ width: "85%", paddingLeft: "1%" }}>I have been interested in Cryptography for a long time and some of my projects include digital signature methods and essentials such as SSL, SHA, AES, RSA. </p>
                 </ul>
