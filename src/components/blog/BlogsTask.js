9b44a516a073a90f563f57399403f18ea938774e
Berk Kırtay <berkberk24@gmail.com>
2022-07-17 12:45:13 +0300

    ios h4 alignment error is fixed

--- src/components/blog/BlogsTask.js
+++ src/components/blog/BlogsTask.js
@@ -1,23 +1,10 @@
 import { Link } from "react-router-dom";
 
 const BlogsTask = ({ blog }) => {
-    /*
-    const [move, setMove] = useState(true);
-    const onMove = () => {
-        setMove(true);
-    }
-
-    const onLeave = () => {
-        setMove(false);
-    }
-    onMouseEnter={onMove} onMouseLeave={onLeave}
-    {move && <p style={{ width: "75%", color: "rgb(2, 129, 68)" }}>{blog.text.slice(0, 200)}...</p>}
-    */
-
     return (
         <li style={{ paddingBottom: "0.5%" }} key={parseInt(blog._id.substring(0, 4), 16)}>
             <Link to={"/blog/" + blog._id} >
-                <h4>{blog.title}<span style={{ float: "right", overflow: "hidden" }}>{blog.date}</span></h4>
+                <h4 style={{ display: "inline" }}>{blog.title}<span style={{ float: "right", overflow: "hidden" }}>{blog.date}</span></h4>
                 <p style={{ width: "75%", color: "rgb(2, 129, 68)" }}>{blog.text.slice(0, 200)}...</p></Link>
         </li>
     );
